8aa0621eb388c0044bdfc8763278ddc14e5acc3d
Tevin Sherman <tevinsherman@yahoo.com>
2019-11-04 23:11:17 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,6 +1,5 @@
 # Use this space to code along with the readme
 def say_hello_ten_times
-
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -13,3 +12,5 @@ puts phrase
 puts phrase
 puts phrase
 end
+
+say_hello_ten_times
